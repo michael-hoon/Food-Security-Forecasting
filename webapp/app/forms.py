3f83d4f9d654ab1ffa30ad5c407e254be59c7c41
chicken8848 <mech_4e@hotmail.com>
2023-11-13 15:37:09 +0800

    help me michael

--- webapp/app/forms.py
+++ webapp/app/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectMultipleField, SubmitField
+from wtforms import StringField, SelectMultipleField, SubmitField, SelectField
 from wtforms.validators import DataRequired, ValidationError
+import pandas as pd
 
 class SelectInputOutput(FlaskForm):
-    input = SelectMultipleField(choices=[])
-    output = SelectMultipleField(choices=[])
-    submit = SubmitField('Visualize!')
+    data = 'data/calls.csv'
+    df = pd.read_csv(data)
+    input = SelectMultipleField(choices=df.keys())
+    output = SelectField(choices=df.keys())
+    submit = SubmitField('Apply Parameters')
--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -7,6 +7,18 @@ from flask import render_template, flash, redirect, url_for
 def home():
     '''landing page'''
     form = SelectInputOutput()
+    if form.validate_on_submit():
+        input = form.input.data
+        output = form.output.data
+        flash('Settings Applied')
+        return render_template(
+            'index.jinja2',
+            title = 'FSP-inator by the JKYM Group',
+            description='Discover poverty all around the world!',
+            template = 'home-template',
+            body="",
+            form = form
+        )
     return render_template(
         'index.jinja2',
         title = 'FSP-inator by the JKYM Group',

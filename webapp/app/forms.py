b477bf086be72cd540d58152ff15db780c85635d
chicken8848 <mech_4e@hotmail.com>
2023-11-13 15:43:29 +0800

    added widget

--- webapp/app/forms.py
+++ webapp/app/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired, ValidationError
 import pandas as pd
 
 class SelectInputOutput(FlaskForm):
-    data = 'data/calls.csv'
-    df = pd.read_csv(data)
+    data = 'data/2Ddataset.xlsx'
+    df = pd.read_excel(data)
     input = SelectMultipleField(choices=df.keys())
     output = SelectField(choices=df.keys())
     submit = SubmitField('Apply Parameters')
